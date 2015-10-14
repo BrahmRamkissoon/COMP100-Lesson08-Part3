2eefc8d9e673d8a634ec1623f5d726dce4ceae7d
Brahmadut Ramkissoon <brahm.ramkissoon@gmail.com>
2015-10-14 14:19:00 -0400

    added consts for face, suit , card and display a random card

--- Lesson08-Part3/Program.cs
+++ Lesson08-Part3/Program.cs
@@ -10,24 +10,37 @@ namespace Lesson08_Part3
     {
         static void Main ( string [] args )
         {
-            int[] faces = new int[13];      // faces from 1 to 13
+            // Random number seed
+            Random rnd = new Random();      // creates object of type Random
+            int randomCardIndex = 0;
+
+            //Constants
+            const int FACE_NUM = 13;
+            const int CARD_NUM = 52;
+            const int SUIT_NUM = 4;
+          
+
+            // Arrays Declaration
+            int[] faces = new int[FACE_NUM];      // faces from 1 to 13
             string[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
-            string[] deckOfCards = new string[52]; // deck of cards
+            string[] deckOfCards = new string[CARD_NUM]; // contains suits and faces
+
+            // Card counter
             int card = 0;
 
 
             // Initialize faces array
-            for (int index  = 0; index < faces.Length; index++)
+            for (int index  = 0; index < FACE_NUM; index++)
             {
                 faces[index] = index + 1;       //  faces begin at 1
             }
 
-            // Creates deck of cards
-            for (int suit = 0; suit < suits.Length; suit++)
+            // Create deck of cards
+            for (int suit = 0; suit < SUIT_NUM; suit++)
             {
-                for (int face = 0; face < faces.Length; face++)
+                for (int face = 0; face < FACE_NUM; face++)
                 {
-                    deckOfCards[card] = faces[face].ToString() + "of" + suits[suit];
+                    deckOfCards[card] = faces[face].ToString() + " of " + suits[suit];
                     card++;  // 0 to 51 ( 52 cards )
                 } // inner loop
             } // outer loop
@@ -38,6 +51,13 @@ namespace Lesson08_Part3
                 Console.WriteLine( i );
             }
 
+            // Display a random card
+            Console.WriteLine();
+            randomCardIndex = rnd.Next( 52 );   // get the next random number from the pool
+            Console.WriteLine( deckOfCards[randomCardIndex] );
+
+
+            // End of program
             Console.WriteLine(  );
             Console.WriteLine( "+++++++++++++++++++++" );
             Console.WriteLine( "Press any key to continue...." );
